ebe1932cf64dbd803163e88ee400cb363442e504
Kaiser Dandangi <kaiser.dandangi@instacart.com>
2022-07-29 15:27:36 -0400

    usability improvements

--- script.js
+++ script.js
@@ -54,3 +54,17 @@ app.controller('MainCtrl', function($scope){
 
   $scope.error = ''
 })
+
+app.directive('ngEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if(event.which === 13) {
+        scope.$apply(function(){
+          scope.$eval(attrs.ngEnter, {'event': event});
+        });
+
+        event.preventDefault();
+      }
+    });
+  };
+});
